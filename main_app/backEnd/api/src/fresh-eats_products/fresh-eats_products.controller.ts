37893b95cfdc5d0c7bc9c8c7e0dab978c137c8d9
Shayanta Shopnil <shopnil710@gmail.com>
2023-11-28 22:41:16 +0600

    changes in backend

--- main_app/backEnd/api/src/fresh-eats_products/fresh-eats_products.controller.ts
+++ main_app/backEnd/api/src/fresh-eats_products/fresh-eats_products.controller.ts
@@ -4,16 +4,16 @@ import { FreshEatsProductsService } from './fresh-eats_products.service';
 @Controller('fresh-eats-products')
 export class FreshEatsProductsController {
 
-    @Post()
-    createPost(
-        @Body('seller_id') seller_id : number,
-        @Body('name') name : string,
-        @Body('price') price: number,
-        @Body('img_src') img_src: string,
-        @Body('rem_item') rem_item: number,
-        @Body('description') description?: string,
-        @Body('rating') rating?: number,
-    ){
-        return this.FreshEatsProductsService
-    }
+    // @Post()
+    // createPost(
+    //     @Body('seller_id') seller_id : number,
+    //     @Body('name') name : string,
+    //     @Body('price') price: number,
+    //     @Body('img_src') img_src: string,
+    //     @Body('rem_item') rem_item: number,
+    //     @Body('description') description?: string,
+    //     @Body('rating') rating?: number,
+    // ){
+    //     return this.FreshEatsProductsService
+    // }
 }
