bd2a64cb6839fb0d3e68638d87fe403d702ade2f
Shayanta Shopnil <shopnil710@gmail.com>
2023-12-09 08:52:56 +0600

    changed global keys

--- main_app/frontEnd/globalkeys.tsx
+++ main_app/frontEnd/globalkeys.tsx
@@ -1,4 +1,4 @@
 export enum GLOBALKEYS{
-    myIp4Addr = 'http://192.168.0.103:3000'//copy this and comment 
+    myIp4Addr = 'http://10.10.226.16:3000'//copy this and comment 
     //paste the same variable with diff ip address
 }
